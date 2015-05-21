9e0d433ecf4f0e3123348a97a49000119a7ad567
Guangshuo CHEN <guangshuo.chen@inria.fr>
2015-05-21 12:47:02 +0200

    h5v2 gitignore

---
+++ h5v2/Gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var csso = require('gulp-csso');
+var rename = require("gulp-rename");
+var htmlmin = require('gulp-htmlmin');
+// var less = require("gulp-less");
+var del = require('del');
+var gm = require('gulp-gm');
+var imagemin = require('gulp-imagemin');
+var jade = require('gulp-jade');
+var toJson = require('gulp-to-json');
+
+
+gulp.task('tojson',function(){
+   	return gulp.src('slides_min/*.png').pipe(toJson({
+   		relative:true
+    }));
+})
+
+
+gulp.task('image',function(){
+	return gulp.src("slides/*.png")
+		.pipe(imagemin({}))
+        .pipe(gulp.dest('./slides_min'));
+});
+
+
+gulp.task('html',function(){
+	var slides = require('./output.json')
+	return gulp.src('./templates/index.jade')
+    	.pipe(jade({
+	      	data:{
+	      		slides:slides
+	      	},
+   		 }))
+    	.pipe(gulp.dest('./'));
+})
+
